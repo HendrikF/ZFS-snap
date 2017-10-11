bf07467cc762a8491bed06da4c550072a7f84704
Hendrik Fritsch <hendrik.fritsch@web.de>
2017-10-12 01:36:37 +0200

    Heavily reduce features and remove all the inconsistencies

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # ZFS Snap 
 
 ZFS Snap is the idea of using ZFS+CRON+BASH to create a one-stop solution for automatic rolling snapshots.
-This script was insipred by [AlBlue][http://alblue.bandlem.com/2008/11/crontab-generated-zfs-snapshots.html].
 
 ## License
 
@@ -9,81 +8,56 @@ ZFS Snap is free software and licensed under the GNU GPLv3 or later. You
 are welcome to change and redistribute it under certain conditions. For more
 information see the LICENSE file or visit http://www.gnu.org/licenses/gpl-3.0.html
 
-
 ## Running ZFS Snap
 
-
 The script offers several switches to change the default behaviour, which is simply taking a snapshot with a timestamp.
-Only a few standard programs are needed, which should be installed by default on any \*nix/Linux system. In my case
-FreeBSD, which does not ship with bash by default, but can be installed from the ports.
 
 Requirements:
 
 ```
-zpool & zfs
-tr
 bash
-cut
-trail
-e-/grep
+zfs
+tail
+grep
 date
 sort
 xargs
 ```
 
-The only setup part is checking the header of the script and replacing the binary paths with the ones on your system using which. 
-If you are running FreeBSD, this will probably work with the supplied script OOTB.
-
 Options:
 
 ```
+-h usage help
 -d <default option> : hourly, daily, weekly, monthly, yearly
 -l <label> default: Automatic
--p pretend, no snapshot creation or deletion
--v verbose (not implemented)
--h help (not implemented)
--u <property to look for>=<value> default: com.sun:auto-snapshot=true
 -r <number> : number of how many of those backups to retain ; default: 10
 ```
 
-The -p switch emulates a 'pretend' action, printing out the actions taken by the script.
 Five default behaviours have been implemented with following settings for label prefix and retention
-hourly: AutoH 24 
-daily: AutoD 7
-weekly: AutoW 4
-monthly: AutoM 12
-yearly: AutoY 10
-Default retentions have precedence over user retention numbers.
+
+```
+hourly:  h 48 
+daily:   d 14
+weekly:  w  4
+monthly: m 12
+yearly:  y  5
+```
+
+These default values are overridden by user values.
 
 ## Crontab
 
-As you will most likely have to be logged in as root to use the zfs and zpool binaries. To change the crontab you will have to issue a
-crontab -e
-as root and enter following lines as  examples into your crontab editor to allow for the default behaviours
+```
+PATH=/bin:/sbin
+@hourly /opt/zfs_snap.sh -d hourly
+@daily /opt/zfs_snap.sh -d daily
+@weekly /opt/zfs_snap.sh -d weekly
+@monthly /opt/zfs_snap.sh -d monthly
+@yearly /opt/zfs_snap.sh -d yearly
+```
 
-```
-PATH=/etc:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin
-hourly /root/zfs_snap.sh -d hourly
-@daily /root/zfs_snap.sh -d daily
-@weekly /root/zfs_snap.sh -d weekly
-@monthly /root/zfs_snap.sh -d monthly
-@yearly /root/zfs_snap.sh -d yearly
-```
-The change to the PATH variable is optional, but necessary on FreeBSD. Other systems have not been tested.
+The change to the PATH variable might be needed on some systems.
 
 ## ZFS properties
 
-I started my ZFS raid under Open Solaris and saw that my initial raid had a strange property that would
- not pop up anywhere apart from Solaris documentation: com.sun:auto-snapshot.
-I use this property to determine if I back up a ZFS filesystem or not, but as ZFS allows for user properties,
-you can create and use your own ones as well, once it is implemented. 
-
-
-## Shortcomings
-
-Many, but I put this together with the best interest to users like me, who have a fairly small ZFS raid setup and who wish to keep
-rolling snapshots without the need to worry about them too much.
-
-## Finally
-
-I hope this script is making other people's backup easier and maybe you have an idea on how to improve on this script, apart from that have fun! :)
+The ZFS property `com.sun:auto-snapshot` determines if a ZFS dataset is snapshoted or not.
--- zfs_snap.sh
+++ zfs_snap.sh
@@ -1,168 +1,103 @@
 #!/usr/bin/env bash
 #
-# zfs_snap.sh version v 0.2 2012-04-16
+# zfs_snap.sh
 # Copyright 2012 Nils Bausch
-# License GNU AGPL 3 or later
+# License: GNU GPL Version 3 or later
 #
 # take ZFS snapshots with a time stamp
-# -h help page
-# -d choose default options: hourly, daily, weekly, monthly, yearly
-# -l the common label to be used if requested
-# -v verbose output 
-# -p pretend - don't take snapshots
-# -u <property>=<value> for user chosen ZFS properties
 
-# Path to binaries used 
-ZPOOL="/sbin/zpool"
-ZFS="/sbin/zfs"
-EGREP="/usr/bin/egrep"
-GREP="/usr/bin/grep"
-TAIL="/usr/bin/tail"
-SORT="/usr/bin/sort"
-XARGS="/usr/bin/xargs"
-DATE="/bin/date"
-CUT="/usr/bin/cut"
-TAIL="/usr/bin/tail"
-TR="/usr/bin/tr"
+usage () {
+    [ "$#" -ne 0 ] && echo "$@" >&2
+    echo "Usage: $(basename $0) [-h] [-d presetName] [-l labelprefix] [-r <num>]" >&2
+    exit 2
+}
 
 # property used to check if auto updates should be made or not
 SNAPSHOT_PROPERTY_NAME="com.sun:auto-snapshot"
 SNAPSHOT_PROPERTY_VALUE="true"
 
-# test for number of supplied arguments if zero, complain
-if [ $# -eq 0 ]; then
-	printf 'usage: %s [-h] [-d presetName] [-l label] [-v] [-p] \n' $(basename $0) >&2
-        exit 2
-fi
+[ "$#" -eq 0 ] && usage
 
-# set default values 
-DEFAULTOPT=
+PRESET=
 LABELPREFIX="Automatic"
-LABEL=`${DATE} +"%FT%H:%M"`
-vflag=
-pflag=
-retention=10
-userproperty=
-exclude=
+LABEL="$(date +"%Y%m%d-%H%M%S")"
+RETENTION=10
 
-# go through passed options and assign to variables
-while getopts 'hd:l:vpu:e:' OPTION
-do
-	case $OPTION in
-	h) 	# help goes here ... somehow 
-		;;
-	d) 	DEFAULTOPT="$OPTARG"
-		;;
-	l)	LABELPREFIX="$OPTARG"
-		;;
-	v) 	vflag=1
-		;;
-	p) 	pflag=1
-		;;
-	u)	userproperty="$OPTARG"
-		;;
-	r)	retention="$OPTARG"
-		;;	
-	e)      exclude="$OPTARG"
-		;;
-	?)    	printf "Usage: %s: [-h] [-d <default-preset>] [-v] [-p] [-u <property>=<value>] [-r <num>]\n" $(basename $0) >&2
-		exit 2
-		;; 
-	esac
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+        -h | --help)
+            usage
+            ;;
+        -d | --default)
+            [ "$#" -ge 2 ] || usage "Missing argument"
+            shift
+            PRESET="$1"
+            ;;
+        -l | --label)
+            [ "$#" -ge 2 ] || usage "Missing argument"
+            shift
+            USER_PREFIX="$1"
+            ;;
+        -r | --retention)
+            [ "$#" -ge 2 ] || usage "Missing argument"
+            shift
+            USER_RETENTION="$1"
+            ;;
+        *)
+            echo "Unknown argument: $1" >&2
+            usage
+            ;;
+    esac
+    shift
 done
 
 # go through possible presets if available
-if [ -n "$DEFAULTOPT" ]; then
-	case $DEFAULTOPT in
-	hourly)	LABELPREFIX="AutoH"
-		LABEL=`${DATE} +"%FT%H:%M"`
-		retention=24
-		;;
-	daily) 	LABELPREFIX="AutoD" 
-		LABEL=`${DATE} +"%F"`
-		retention=7
-		;;
-	weekly)	LABELPREFIX="AutoW"
-		LABEL=`${DATE} +"%Y-%U"`	
-		retention=4	
-		;;
-	monthly)LABELPREFIX="AutoM"
-		LABEL=`${DATE} +"%Y-%m"`
-		retention=12
-		;;
-	yearly)	LABELPREFIX="AutoY"
-		LABEL=`${DATE} +"%Y"`	
-		retention=10
-		;;
-	*)	printf 'Default option not specified\n'
-		exit 2
-		;; 
-	esac
+if [ -n "$PRESET" ]; then
+    case "$PRESET" in
+    hourly)
+        LABELPREFIX="h"
+        RETENTION=48
+        ;;
+    daily)
+        LABELPREFIX="d"
+        RETENTION=14
+        ;;
+    weekly)
+        LABELPREFIX="w"
+        RETENTION=4
+        ;;
+    monthly)
+        LABELPREFIX="m"
+        RETENTION=12
+        ;;
+    yearly)
+        LABELPREFIX="y"
+        RETENTION=5
+        ;;
+    *)  echo "Unknown default option" >&2
+        exit 1
+        ;;
+    esac
 fi
 
-# set user property if given
-if [ -n "$userproperty" ]; then
-	set -- `echo "$userproperty" | tr '=' ' '`
-	SNAPSHOT_PROPERTY_NAME="$1"
-	SNAPSHOT_PROPERTY_VALUE="$2"
-fi
+# if these variables are set they will override the preset
+[ -z "${USER_PREFIX+x}" ] || LABELPREFIX="$USER_PREFIX"
+[ -z "${USER_RETENTION+x}" ] || RETENTION="$USER_RETENTION"
 
-# available pools for backup: zpool list - excludes 
-ALLPOOLS=`${ZPOOL} list | ${TAIL} -n +2 | ${CUT} -d' ' -f1 | tr '\n' ' '` 
-for item in ${exclude//,/ }; do
-	ALLPOOLS=`echo $ALLPOOLS | sed -e s/^"${item}"[^:alnum:.:-]//g -e s/[^:alnum:.:-]"${item}"[^:alnum:.:-]/\ /g -e s/[^:alnum:.:-]"${item}"$//g`
-done
-# now create a pattern for egrep to match with 
-ALLPOOLS=`echo ${ALLPOOLS} |sed 's/[a-zA-Z0-9._:-]*/\^&\$|\^&\//g' | sed 's/ /|/g'`
-
-# determine if any of the pools are busy, if yes abort and print error
-POOLS_OK=`${ZPOOL} status | ${EGREP} -c "scrub completed|none requested|No known data errors"`
-if [ $POOLS_OK -eq 0 ]; then
-	printf 'Pool(s) busy, no snapshots taken.\n'
-        exit 2
-fi  
+# available datasets for backup
+readarray -t datasets < <(zfs list -H -o name)
 
 #TAKE SNAPSHOTS
-
-# get a list of all available zfs filesystems by listing them and then look for property and take snapshots
-for i in $(${ZFS} list | ${TAIL} -n +2 | ${TR} -s " " | ${CUT} -f 1 -d ' '| ${EGREP} -i "${ALLPOOLS}") ; do
-        # get state of auto-snapshot property, either true or false
-	if [ "$vflag" ]; then
-		echo  "${ZFS} get ${SNAPSHOT_PROPERTY_NAME} $i | ${TAIL} -n 1 | ${TR} -s ' ' | ${CUT} -f 3 -d ' '"
-	 	VALUE=`${ZFS} get ${SNAPSHOT_PROPERTY_NAME} $i | ${TAIL} -n 1 | ${TR} -s ' ' | ${CUT} -f 3 -d ' '`	
-	else
-	        VALUE=`${ZFS} get ${SNAPSHOT_PROPERTY_NAME} $i | ${TAIL} -n 1 | ${TR} -s ' ' | ${CUT} -f 3 -d ' '`
-	fi
-       	# do the snapshot dance
-       	if [ $VALUE = $SNAPSHOT_PROPERTY_VALUE ]; then
-		if [ "$pflag" ]; then
-			echo ${ZFS} snapshot $i@$LABELPREFIX-$LABEL
-		else
-               		$(${ZFS} snapshot $i@$LABELPREFIX-$LABEL)
-		fi
-       	fi
+for dataset in "${datasets[@]}"; do
+    # get value of auto-snapshot property, either true or false
+    VALUE="$(zfs get "$SNAPSHOT_PROPERTY_NAME" "$dataset" -H -o value)"
+    if [ "$VALUE" = "$SNAPSHOT_PROPERTY_VALUE" ]; then
+        zfs snapshot "$dataset@$LABELPREFIX-$LABEL"
+    fi
 done
 
-
 #DELETE SNAPSHOTS
-# adjust retention to work with tail i.e. increase by one
-let retention+=1
-for pool in $(${ZPOOL} list | ${TAIL} -n +2 | ${CUT} -d' ' -f1 | ${EGREP} -i "${ALLPOOLS}"); do
-	if [ "$vflag" ]; then
-		echo "${ZFS} list -t snapshot -o name | ${GREP} $pool@${LABELPREFIX} | ${SORT} -r | ${TAIL} -n +${retention}"
-		list=`${ZFS} list -t snapshot -o name | ${GREP} $pool@${LABELPREFIX} | ${SORT} -r | ${TAIL} -n +${retention}`	
-	else
-		list=`${ZFS} list -t snapshot -o name | ${GREP} $pool@${LABELPREFIX} | ${SORT} -r | ${TAIL} -n +${retention}`
-	fi
-	
-	if [ "$pflag" ]; then
-		if [ -n "$list" ]; then
-			echo "Delete recursively:" 
-			echo "$list"
-		else
-			echo "No snapshots to delete for pool ${pool}"
-		fi
-	else
-		$(${ZFS} list -t snapshot -o name | ${GREP} $pool@${LABELPREFIX} | ${SORT} -r | ${TAIL} -n +${retention} | ${XARGS} -n 1 ${ZFS} destroy -r)
-	fi
+for dataset in "${datasets[@]}"; do
+    # grep for prefix and a dash to not accidentally match anything else
+    zfs list -t snapshot -H -o name | grep "$dataset@$LABELPREFIX-" | sort -r | tail -n +$((RETENTION+1)) | xargs -n 1 -r zfs destroy -r
 done
